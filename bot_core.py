1b7a22882289ce56edc3502b314b06d7509f5768
Vladimir <dog67688@gmail.com>
2022-03-02 18:12:45 +0300

    Custom settings implemented

--- bot_core.py
+++ bot_core.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 import telebot as tb
 
@@ -10,26 +11,51 @@ bot = tb.TeleBot(token)
 user_functions = {}
 
 
+def write_config(chat_id, case_sensitive: bool = True, custom_consts: bool = True):
+    print(f"Writing {case_sensitive=}, {custom_consts=}")
+    with open(f"userData\\{chat_id}.dat", mode="wb") as f:
+        barr = bytes([case_sensitive, custom_consts])
+        print(barr)
+        f.write(barr)
+        f.flush()
+
+
+def get_config(chat_id):
+    with open(f"userData\\{chat_id}.dat", mode="rb") as f:
+        barr = f.read(2)
+        print(barr)
+        case_sensitive = bool(barr[0])
+        custom_consts = bool(barr[1])
+    print(f"Reading {case_sensitive=}, {custom_consts=}")
+    return case_sensitive, custom_consts
+
+
 def generator(chat_id: int):
-    bot.send_message(chat_id, "Input constants (name=value): ")
-    inp = yield
     var_dict = {}
-    while (inp != '.') and (inp.strip() != ""):
-        name, val = inp.split('=')
-        name.strip()
-        val.strip()
-        var_dict[name] = val
+    case_sensitive, custom_consts = get_config(chat_id)
+    if custom_consts:
         bot.send_message(chat_id, "Input constants (name=value): ")
         inp = yield
+        while (inp != '.') and (inp.strip() != ""):
+            name, val = inp.split('=')
+            name.strip()
+            name = name if case_sensitive else name.lower()
+            val.strip()
+            var_dict[name] = val
+            bot.send_message(chat_id, "Input constants (name=value): ")
+            inp = yield
 
     bot.send_message(chat_id, "Input function: ")
     s = yield
     s = f"({s})"
+    s = s if case_sensitive else s.lower()
     bot.send_message(chat_id, "Left limit: ")
     a = yield
+    a = a if case_sensitive else a.lower()
     a = str_to_float(var_dict)(a)
     bot.send_message(chat_id, "Right limit: ")
     b = yield
+    b = b if case_sensitive else b.lower()
     b = str_to_float(var_dict)(b)
     print()
     polish = translate(s)
@@ -43,13 +69,15 @@ def generator(chat_id: int):
         print(err.args)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'run', 'calc'])
 def start_msg(message):
-    uid = message.from_user.first_name
-    bot.reply_to(message, f"Hello, {uid}")
-    gen = generator(message.chat.id)
+    bot.reply_to(message, f"Hello, {message.from_user.first_name}")
+    chat_id = message.chat.id
+    if not os.path.isfile(f"userData\\{chat_id}.dat"):
+        write_config(chat_id)
+    gen = generator(chat_id)
     next(gen)
-    user_functions.update({message.chat.id: gen})
+    user_functions.update({chat_id: gen})
 
 
 def calc_step(msg):
@@ -61,6 +89,35 @@ def calc_step(msg):
         print(user_functions[msg.chat.id].send(msg.text))
     except StopIteration:
         user_functions.pop(msg.chat.id)
+
+
+@bot.message_handler(commands=['settings', 'config'])
+def settings(msg):
+    case_sensitive, custom_consts = get_config(msg.chat.id)
+    response = f"Settings list: \n" \
+               f"1) /register \n" \
+               f"2) /customConsts \n" \
+               f"\n" \
+               f"Current settings: \n" \
+               f"Case sensitive: {case_sensitive}\n" \
+               f"Custom constants: {custom_consts}"
+    bot.reply_to(msg, response)
+
+
+@bot.message_handler(commands=['register'])
+def register(msg):
+    case_sensitive, custom_consts = get_config(msg.chat.id)
+    case_sensitive = not case_sensitive
+    bot.reply_to(msg, f"Case sensitive: {case_sensitive}")
+    write_config(msg.chat.id, case_sensitive, custom_consts)
+
+
+@bot.message_handler(commands=['customConsts'])
+def custom_constants(msg):
+    case_sensitive, custom_consts = get_config(msg.chat.id)
+    custom_consts = not custom_consts
+    bot.reply_to(msg, f"Custom constants: {custom_consts}")
+    write_config(msg.chat.id, case_sensitive, custom_consts)
 
 
 @bot.message_handler()
